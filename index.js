5bab274f97d1edb6dd48131b6dcca91b6b9c08fb
Lev Taydakov <lev.taydakov@gmail.com>
2014-11-02 00:53:11 -0700

    completed version 0.0.1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var express = require('express')
+var app = express();
 var nodemailer = require('nodemailer');
-var settings = require('./settings');
+var settings = require('./settings'); /* should contain service (e.g. Gmail), user, pass */
 
 var transporter = nodemailer.createTransport({
   service: settings.service,
@@ -9,17 +11,36 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-var mailOptions = {
-  from: "Taydakov.com <contact.taydakov.com@gmail.com>",
-  to: "lev.taydakov@gmail.com",
-  subject: "Hello!",
-  text: "What's up!",
-};
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Message sent: ' + info.response);
-  }
-});
+app.get('/', function(request, response) {
+  if (!request.query.name || !request.query.email || !request.query.subject || !request.query.message)
+    return response.send('error');
+
+  var from = request.query.name + " <" + request.query.email + ">";
+  var subject = from + " - " + request.query.subject;
+  var text = request.query.message;
+
+  var mailoptions = {
+    to: "lev.taydakov@gmail.com",
+    subject: subject,
+    text: text
+  };
+
+  console.log(mailoptions);
+
+  transporter.sendMail(mailoptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      response.send(request.query.callback + "({result: false})");
+    } else {
+      console.log('Message sent: ' + info.response);
+      response.send(request.query.callback + "({result: true, message: \"You message has been sent. Thank you!\"})");
+    }
+  });
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
